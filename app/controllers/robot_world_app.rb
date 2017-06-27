16b5906bec161e213257f404240b487557da2125
marcoverzero <marcoverzero@gmail.com>
2017-06-27 17:32:16 -0600

    blank file db breaks stuff

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -7,12 +7,21 @@ class RobotWorldApp < Sinatra::Base
     erb :index
   end
 
+  get '/robots/new' do
+    erb :new
+  end
+
   get '/robots/:id' do
-    @robot = Robot.find(params[:id])
+    @robot = Robot.find_by(params[:id])
     erb :show
   end
 
-  
+  post '/robots' do
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect '/robots'
+  end
+
 
 
 
