7251e76977f5b0dbed5f2a64d1707df05eec16c4
marcoverzero <marcoverzero@gmail.com>
2017-06-27 14:59:30 -0600

    rb migration file

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -6,5 +6,19 @@ class RobotWorldApp < Sinatra::Base
   get '/robots' do
     erb :index
   end
-end
 
+  get '/robots/:id' do
+    @robot = Robot.find(params[:id])
+    erb :show
+  end
+
+  
+
+
+
+  not_found do
+    erb :error
+  end
+
+
+end
